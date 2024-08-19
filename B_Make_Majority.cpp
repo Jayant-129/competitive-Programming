5d1f90a31f59300e6520d29cc7bbd486e4082caf
Jayant Agrawal <agrawaljayant86@gmail.com>
2024-08-19 21:48:15 +0530

    Add ./B_Make_Majority.cpp

---
+++ B_Make_Majority.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void solve(){
+    ll n;
+    cin >> n;
+    string s;
+    cin >>  s;
+    
+}
+
+int main(){
+  ll t;
+  cin >> t;
+  while(t-- > 0){
+    solve();
+  }
+}
