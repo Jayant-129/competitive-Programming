96205cae758143a25de4d872461bc47a44ce2c22
Jayant Agrawal <agrawaljayant86@gmail.com>
2024-09-01 20:05:58 +0530

    Add ./A_Sakurako_s_Exam.cpp

---
+++ A_Sakurako_s_Exam.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+void solve(){
+    ll a, b;
+    cin >> a >> b;
+    b = b%2;
+    if(b == 0){
+        if(a % 2 == 0){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    else{
+        if(a % 2 == 0 && a > 0){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+
+}
+
+int main(){
+  ll t;
+  cin >> t;
+  while(t-- > 0){
+    solve();
+  }
+}
