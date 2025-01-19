a5e7e54c0d16bee0f6b73962c44a2ab5ba78609d
Jayant Agrawal <agrawaljayant86@gmail.com>
2025-01-19 09:32:35 +0530

    Add ./D_Secret_Santa.cpp

---
+++ D_Secret_Santa.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    ll n; cin >> n;
+
+    vector<ll> v(n + 1, -1);
+    for(int i = 1; i <= n; i++){
+        cin >> v[i];
+    }
+}
+
+int main(){
+  ll t; cin >> t;
+  while(t--){
+    solve();
+  }
+}
