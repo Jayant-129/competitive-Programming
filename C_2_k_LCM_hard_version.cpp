048a086c79e15a8563660e4303c9417b2d7f6e18
Jayant Agrawal <agrawaljayant86@gmail.com>
2025-01-24 17:17:48 +0530

    Add ./C_2_k_LCM_hard_version.cpp

---
+++ C_2_k_LCM_hard_version.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    ll n, k; cin >> n >> k;
+    for(int i = 1; i < k; i++){
+        cout << 1 << " ";
+    }
+}
+
+int main(){
+  ll t; cin >> t;
+  while(t--){
+    solve();
+  }
+}
