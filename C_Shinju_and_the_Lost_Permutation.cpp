941b73dca4753f1bffb98ec580f92a3799f7215a
Jayant Agrawal <agrawaljayant86@gmail.com>
2025-03-22 11:55:56 +0530

    Add ./C_Shinju_and_the_Lost_Permutation.cpp

---
+++ C_Shinju_and_the_Lost_Permutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    ll n; cin >> n;
+    vector<ll> v(n); for(auto& x: v) cin >> x;
+}
+
+int main(){
+  ll t; cin >> t;
+  while(t--){
+    solve();
+  }
+}
