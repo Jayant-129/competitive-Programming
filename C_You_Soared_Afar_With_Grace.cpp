c5d51021639a93052c7dc549d4be8867f0e7989e
Jayant Agrawal <agrawaljayant86@gmail.com>
2025-04-07 21:22:49 +0530

    imporved

--- C_You_Soared_Afar_With_Grace.cpp
+++ C_You_Soared_Afar_With_Grace.cpp
@@ -9,7 +9,6 @@ void solve(){
     for(auto &x: a) cin >> x;
     for(auto &x: b) cin >> x;
     
-    // Check condition on self-pairs.
     int selfCount = 0, selfIndex = -1;
     for (int i = 0; i < n; i++){
         if(a[i] == b[i]){
@@ -29,11 +28,7 @@ void solve(){
         }
     }
     
-    // For non-self indices check complementary existence:
-    // For each i with a[i]!=b[i], there must be some j such that (a[j], b[j]) = (b[i], a[i]).
-    // We can count frequencies of ordered pairs.
     unordered_map<ll, int> freq;
-    // We'll encode a pair (x, y) as: key = x*(n+1) + y.
     for(int i = 0; i < n; i++){
         if(a[i]==b[i]) continue;
         ll key = a[i]*(n+1LL) + b[i];
@@ -48,9 +43,7 @@ void solve(){
             return;
         }
     }
-    
-    // Build a mapping from element value to its index in array a.
-    // Since a is a permutation, this mapping is unique.
+
     vector<int> pos(n+1);
     for (int i = 0; i < n; i++){
         pos[a[i]] = i;
@@ -58,39 +51,30 @@ void solve(){
     
     vector<pair<int,int>> ans;
     
-    // If n is odd, move the unique self pair to the center
     if(n % 2 == 1){
-        int center = n/2;  // 0-indexed center
+        int center = n/2;
         if(selfIndex != center){
-            // Swap elements at selfIndex and center in both a and b.
             swap(a[selfIndex], a[center]);
             swap(b[selfIndex], b[center]);
-            // Update positions in pos mapping: update for the swapped values.
             pos[a[selfIndex]] = selfIndex;
             pos[a[center]] = center;
             ans.push_back({selfIndex, center});
         }
     }
-    
-    // Process indices 0 to floor(n/2)-1.
-    // For i in left half, we want to force: a[i] should equal b[n-1-i] (i.e. b at mirror position)
-    // by swapping the element in a that equals b[i] to position n-1-i.
+  
     for (int i = 0; i < n/2; i++){
         int right = n - 1 - i;
-        // Find j, the index in a where element equals b[i].
+
         int j = pos[b[i]];
-        // Swap the element at j with the element at right if needed.
         if(j != right){
             swap(a[j], a[right]);
             swap(b[j], b[right]);
-            // Update pos mapping for the swapped elements.
             pos[a[j]] = j;
             pos[a[right]] = right;
             ans.push_back({j, right});
         }
     }
     
-    // Final verification: for all i, a[i] should equal b[n-1-i]
     for (int i = 0; i < n; i++){
         if(a[i] != b[n - 1 - i]){
             cout << -1 << "\n";
@@ -98,10 +82,8 @@ void solve(){
         }
     }
     
-    // Output the swaps.
     cout << ans.size() << "\n";
     for(auto &p: ans){
-        // Output in 1-indexed format.
         cout << p.first + 1 << " " << p.second + 1 << "\n";
     }
 }
